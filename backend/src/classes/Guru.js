8afb436e716e7ce8573ebe761436cf475103004b
Sandres7 <sandresparuliansitorus@gmail.com>
2026-04-27 18:09:37 +0700

    ubah admin, feedback, guru

--- backend/src/classes/Guru.js
+++ backend/src/classes/Guru.js
@@ -1,17 +1,13 @@
 const User = require('./User');
+const Availability = require('./Availability'); // Pastikan ini di-require
+const Feedback = require('./Feedback');
 
 class Guru extends User{
   #portofolio;
   #daftarMateri;
   #jadwalAvailable;
-<<<<<<< HEAD
   constructor(username, email, password, nama_user) {
     super(username, email, password, nama_user);
-=======
-  #totalSesi;
-  constructor(username, email, password, nama_user, id = null){
-    super(username, email, password, nama_user, id);
->>>>>>> cc15681ea6ded3d0928ea503e0ac860d859b3e15
     this.portfolio        = [];
     this.daftarMateri     = [];
     this.jadwalAvailable  = [];
