da84564d6e4245d335192e3431a89b9a0e1e8c4a
nyaw216 <naufalhanif777@gmail.com>
2026-04-27 16:55:16 +0700

    Update registerController, appRouting, registerService

--- backend/src/controllers/registerController.js
+++ backend/src/controllers/registerController.js
@@ -3,7 +3,7 @@ const Guru = require('../classes/Guru');
 const Murid = require('../classes/Murid');
 
 const register = async (req, res) => {
-    const { namaLengkap, email, password, role } = req.body;
+    const { namaLengkap, email, password, role, username = null } = req.body;
     let conn;
 
     try {
@@ -23,24 +23,29 @@ const register = async (req, res) => {
             });
         }
 
+        let newUserInstace;
+
         if (role === 'Guru') {
-            const queryGuru = `INSERT INTO guru (nama_guru, email_guru, password) VALUES (?, ?, ?)`;
-            await conn.query(queryGuru, [namaLengkap, email, password]);
+            const queryGuru = `INSERT INTO guru (nama_guru, email_guru, password, username) VALUES (?, ?, ?, ?)`;
+            const result = await conn.query(queryGuru, [namaLengkap, email, password, username]);
             
-            const baru = new Guru(null, email, email, password, namaLengkap);
-            
+            const insertedId = result.insertedId;
+            newUserInstance = new Guru(username, email, password, namaLengkap, insertedId);
+
         } else if (role === 'Murid') {
-            const queryMurid = `INSERT INTO murid (nama_murid, email, password) VALUES (?, ?, ?)`;
-            await conn.query(queryMurid, [namaLengkap, email, password]);
+            const queryMurid = `INSERT INTO murid (nama_murid, email, password, username) VALUES (?, ?, ?, ?)`;
+            const result = await conn.query(queryMurid, [namaLengkap, email, password]);
             
-            const baru = new Murid(null, email, email, password, namaLengkap);
+            const insertedId = result.insertId;
+            newUserInstance = new Murid(username, email, password, namaLengkap, insertedId);
         } else {
             return res.status(400).json({ success: false, message: "Role tidak valid!" });
         }
 
         res.status(201).json({
             success: true,
-            message: "Registrasi berhasil"
+            message: "Registrasi berhasil",
+            data: newUserInstance.getProfile()
         });
 
     } catch (err) {
