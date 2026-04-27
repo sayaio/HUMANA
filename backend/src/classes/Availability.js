143bd2567dfe0a6bb60fc647aa2f6387a5f64f00
Sandres7 <sandresparuliansitorus@gmail.com>
2026-04-27 18:14:03 +0700

    ubah admin, feedback, guru

--- backend/src/classes/Availability.js
+++ backend/src/classes/Availability.js
@@ -1,20 +1,29 @@
 class Availability {
-    #idAvailability;
+    // 1. Deklarasi (Harus ada di sini)
+    #id_availability; 
     #waktuMulai;
     #waktuSelesai;
+
     constructor(waktuMulai, waktuSelesai) {
+        // Gunakan nama yang sama dengan deklarasi di atas
+        this.#id_availability = Date.now(); 
         this.#waktuMulai = new Date(waktuMulai);
         this.#waktuSelesai = new Date(waktuSelesai);
     }
+
     isAvailable(waktuPesan) {
         return waktuPesan < this.#waktuSelesai && waktuPesan > this.#waktuMulai;
     }
+
+    // 2. Getter (Pastikan nama setelah tanda # sama dengan deklarasi)
     get idAvailability() {
         return this.#id_availability;
     }
+
     get waktuMulai() {
         return this.#waktuMulai;
     }
+
     get waktuSelesai() {
         return this.#waktuSelesai;
     }
