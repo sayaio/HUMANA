b34a4592d01dabe723059d306539a699e7c7e9ea
Lavmort <fthanakbar@gmail.com>
2026-04-28 15:13:58 +0700

    routing materi

--- backend/appRouting.js
+++ backend/appRouting.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const authController = require('./src/controllers/authController'); // Ini logikanya
-router.post('/login', authController.login);   
+const authController = require('./src/controllers/authController');
+router.post('/login', authController.login);
 
 const registerController = require('./src/controllers/registerController');
-router.post('/register', registerController.register); 
+router.post('/register', registerController.register);
 
+const materiController = require('./src/controllers/MateriController');
+router.get('/materi', materiController.getMateriBySubject);
+router.get('/materi/all', materiController.getAllMateri);
 
 module.exports = router;
