662141543d1d582a24a78ab4c9931d653cb1eaf4
Arkannzz <arkanderaya@gmail.com>
2026-04-28 16:24:02 +0700

    rapihin

--- backend/appRouting.js
+++ backend/appRouting.js
@@ -15,14 +15,8 @@ const materiController = require('./src/controllers/MateriController');
 router.get('/materi', materiController.getMateriBySubject);
 router.get('/materi/all', materiController.getAllMateri);
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 const FeedbackController = require('../controllers/FeedbackController');
 router.post('/', FeedbackController.berikanFeedback);
 router.get('/guru/:id_guru', FeedbackController.getFeedbackByGuru);
-=======
->>>>>>> a37ea484d64041f8a50bca34142285fab21dc795
 
->>>>>>> d176d5be5cd02e8de64c0c70be9895c3948b8494
 module.exports = router;
