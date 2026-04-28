08b515aeb400311eddf89f296654f099c3f10788
Sandres7 <sandresparuliansitorus@gmail.com>
2026-04-28 15:58:23 +0700

    fitur feedback

--- backend/appRouting.js
+++ backend/appRouting.js
@@ -2,18 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('./src/controllers/authController');
-router.post('/login', authController.login);
+router.post('/login', authController.login);   
 
 const registerController = require('./src/controllers/registerController');
-router.post('/register', registerController.register);
+router.post('/register', registerController.register); 
 
-<<<<<<< HEAD
+
 const historyController = require('./src/controllers/historyController');
 router.get('/history/:role/:id', historyController.getHistory);
-=======
+
 const materiController = require('./src/controllers/MateriController');
 router.get('/materi', materiController.getMateriBySubject);
 router.get('/materi/all', materiController.getAllMateri);
 
->>>>>>> b34a4592d01dabe723059d306539a699e7c7e9ea
+const FeedbackController = require('../controllers/FeedbackController');
+router.post('/', FeedbackController.berikanFeedback);
+router.get('/guru/:id_guru', FeedbackController.getFeedbackByGuru);
+
 module.exports = router;
